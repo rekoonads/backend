008d57dcffd5ca8d801e7cc16fa0a5f9ec3faff6
Kunal Mukherjee <kunalmukherjee12@gmail.com>
2024-08-02 17:31:54 +0530

    minute

--- index.js
+++ index.js
@@ -20,22 +20,22 @@ app.use(helment());
 app.use(compression());
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173"); // Replace with your client’s URL
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173"); 
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT,PATCH,DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 });
 
+// app.use(cors({
+//   origin:['http://localhost:5173'],
+//   methods: ['GET','POST','PATCH','PUT','DELETE']
+// }))
+
+
 //Routing
 app.use("/", apiRouter);
 
-// app.use(
-//   cors({
-//     origin: "http://localhost:5173", // Replace with your client’s URL
-//     methods: ["GET", "POST", "PUT", "DELETE"], // Adjust methods as needed
-//     allowedHeaders: ["Content-Type", "application/json"], // Adjust headers as needed
-//   })
-// );
+
 
 // Save server loader function
 let server;
