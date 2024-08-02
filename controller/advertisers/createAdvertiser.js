20bd20dc03b06a519b1a4f7318e4effe5659ee9f
shibu-g <shibugoper1@gmail.com>
2024-08-02 12:26:39 +0530

    uuid added

--- controller/advertisers/createAdvertiser.js
+++ controller/advertisers/createAdvertiser.js
@@ -1,4 +1,5 @@
 import Advertisermodel  from "../../models/Advertiser.js";
+import {v4 as uuidv4} from 'uuid';
 
 export default async (req, res) => {
   try {
@@ -8,8 +9,9 @@ export default async (req, res) => {
     } = req.body;
 
     const new_advertiser = new Advertisermodel({
-      advertiserName,
-      createdBy
+        advertiserId: `adv_${uuidv4()}`,
+        advertiserName,
+        createdBy
     });
     await Advertisermodel.create(new_advertiser).then((advertiser) => {
         return res.status(201).json(advertiser);
