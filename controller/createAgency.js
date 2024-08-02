f7003e0c66bbc2ab55fbf891c376d44624fb7ab7
shibu-g <shibugoper1@gmail.com>
2024-08-02 09:08:47 +0530

    agency update

--- controller/createAgency.js
+++ controller/createAgency.js
@@ -1,5 +1,5 @@
-import {Agencymodel} from '../models/Agencies.js';
-import {userModel} from '../models/User.js';
+import { Agencymodel } from '../models/Agencies.js';
+import { userModel } from '../models/User.js';
 
 export default async (req, res) => {
     try {
@@ -8,25 +8,31 @@ export default async (req, res) => {
             name,
             created_by,
             object
-          } = req.body.data;
-        
-          const new_agency =  new Agencymodel({
-            agencyId : id,
-            agencyName: name
-          });
-          console.log(id+"  id " + name+"  name "+object);
-          console.log(new_agency);
+        } = req.body.data || {}; 
 
-          userModel.updateOne(
-              { userId: created_by },
-              { $set: { userType: object } }
-          );
-          let response_data =[];
-          Agencymodel.create(new_agency).then((agency) => {
-             response_data.push({agency_data: agency});
-          });
-          return res.status(201).json(response_data);
+        if (!id || !name || !created_by || !object) {
+            return res.status(400).json({ error: 'Missing required fields' });
+        }
+
+        const new_agency = new Agencymodel({
+            agencyId: id,
+            agencyName: name
+        });
+
+        console.log(id + "  id " + name + "  name " + object);
+        console.log(new_agency);
+
+        await userModel.updateOne(
+            { userId: created_by },
+            { $set: { userType: object } }
+        );
+
+        const agency = await Agencymodel.create(new_agency);
+        const response_data = [{ agency_data: agency }];
+
+        return res.status(201).json(response_data);
     } catch (error) {
-        return res.status(401).json({error: error})
+        console.error(error);
+        return res.status(500).json({ error: 'Internal Server Error' });
     }
-  }
+};
