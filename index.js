385c094e45d7e872105bd4004a86b5436d34b937
Michaelazio <kunalmukherjee12@gmail.com>
2024-08-03 04:54:06 +0530

    Campain Endpoint Modification and Succesful data exchange

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(helment());
 app.use(compression());
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173"); // Replace with your client’s URL
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173"); 
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
