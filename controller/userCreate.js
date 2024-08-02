90078b405b3eceeefea1df4f57d89b32dd8c9dfb
shibu-g <shibugoper1@gmail.com>
2024-08-02 09:12:07 +0530

    update user

--- controller/userCreate.js
+++ controller/userCreate.js
@@ -1,39 +1,40 @@
-import {userModel} from '../models/User.js';
+import { userModel } from '../models/User.js';
 
 export default async (req, res) => {
-
     try {
-
         const {
             id,
             first_name,
-            last_name
-          } = req.body.data;
-        
-          const new_user =  new userModel({
-            userId : id,
-            firstName :first_name,
+            last_name,
+            email_addresses
+        } = req.body.data;
+
+        const new_user = new userModel({
+            userId: id,
+            firstName: first_name,
             lastName: last_name,
-            userType : "",
-            email : req.body.data.email_addresses[0].email_address,
-            phoneNo : ""
-          });
-          console.log(new_user);
-          let response_data =[];
-          
-          const user = await userModel.findById(userId);
+            userType: "",
+            email: email_addresses[0].email_address,
+            phoneNo: ""
+        });
 
-          if (user) {
-              console.log(`User with ID ${userId} already exists.`);
-          } else {
-            userModel.create(new_user).then((user) => {
-              response_data.push({user_data: user});
-            });
-          }
+        console.log(new_user);
+        let response_data = [];
 
-          console.log(response_data);
-          return res.status(201).json(response_data);
+        const user = await userModel.findById(id);
+
+        if (user) {
+            console.log(`User with ID ${id} already exists.`);
+            return res.status(409).json({ error: `User with ID ${id} already exists.` });
+        } else {
+            const createdUser = await userModel.create(new_user);
+            response_data.push({ user_data: createdUser });
+        }
+
+        console.log(response_data);
+        return res.status(201).json(response_data);
     } catch (error) {
-        return res.status(401).json({error: error})
+        console.error(error);
+        return res.status(500).json({ error: 'Internal Server Error' });
     }
-  }
+};
