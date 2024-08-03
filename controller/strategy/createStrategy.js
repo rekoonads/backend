9de209a9308947bc36d419ab903f400694b0de79
Kunal Mukherjee <kunalmukherjee12@gmail.com>
2024-08-03 19:02:55 +0530

    server connection

--- controller/strategy/createStrategy.js
+++ controller/strategy/createStrategy.js
@@ -2,12 +2,14 @@ import Strategy from "../../models/Strategy.js";
 
 export default async (req, res) => {
   const newStrategy = new Strategy({
+    userId: req.body.userId,
     strategyId: req.body.strategyId,
     strategyName: req.body.strategyName,
     strategyDailyBudget: req.body.strategyDailyBudget,
     ageRange: req.body.ageRange,
     gender: req.body.gender,
     screens: req.body.screens,
+    selectedChannels: req.body.selectedChannels,
     audiences: req.body.audiences,
     deliveryTimeSlots: req.body.deliveryTimeSlots,
     creatives: req.body.creatives,
--- models/Strategy.js
+++ models/Strategy.js
@@ -2,12 +2,14 @@ import {model, Schema} from 'mongoose';
 
 
 const strategySchema = new Schema({
+    userId: {type:String},
     strategyId: { type: String, required: true, unique: true },
     strategyName: { type: String, required: true },
     strategyDailyBudget: { type: String, required: true },
     ageRange: { type: String, required: true },
     gender: { type: String },
     screens: { type: String, required: true },
+    selectedChannels: [{type:String}],
     audiences: [{ type: Schema.Types.ObjectId, ref: 'Audiences' }],
     deliveryTimeSlots: { type: String, required: true },
     creatives: { type: String }
