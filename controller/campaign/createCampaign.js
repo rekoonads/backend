4327d94edd82359f27121c2f73f28f23c7c7aac8
Kunal Mukherjee <kunalmukherjee12@gmail.com>
2024-08-07 16:55:32 +0530

    deleted redundant code

--- controller/campaign/createCampaign.js
+++ controller/campaign/createCampaign.js
@@ -4,19 +4,10 @@ export default async (req, res) => {
   try {
     console.log("Request received:", req.body);
     const campaignData = req.body;
-
-    
-    // const existingCampaign = await Campaignmodel.findOne({ userId: campaignData.userId });
-    
-    // if (existingCampaign) {
-    
-    //   return res.status(404).json({ status: false, message: "Campaign already exists" });
-    // } else {
-      
       const campaign = new Campaignmodel(campaignData);
       const savedCampaign = await campaign.save();
       return res.status(201).json(savedCampaign);
-    // }
+    
   } catch (err) {
     console.error("Error saving campaign:", err);
     return res.status(400).json({ error: err.message });
