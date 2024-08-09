ce6999b3b3d3349d2558c8fd10f7d0ac02fec4ae
ayan471 <pakhiraayan@gmail.com>
2024-08-09 13:58:09 +0530

    null

--- controller/update_details.js
+++ controller/update_details.js
@@ -1,54 +1,54 @@
-import Advertisermodel  from "../models/Advertiser.js";
-import  Agencymodel  from "../models/Agencies.js";
+import Advertisermodel from "../models/Advertiser.js";
+import Agencymodel from "../models/Agencies.js";
 
 export default async (req, res) => {
   try {
-    
-    if(req.body.typeofuser=="agencies"){
-        const updateData = {
-            agencyName: req.body.agencyName,
-            agencyLogo: req.body.logo,
-            gstNumber: req.body.gstNumber,
-            legalName: req.body.legalName,
-            address: req.body.address,
-            gstCertificate: req.body.gstCertificate,
-            cinNumber: req.body.cinNumber
-          };
-          
-          const agencyId = req.body.agencyId;
-          const result = await Agencymodel.findOneAndUpdate({ agencyId: agencyId }, updateData, { new: true });
-          if (result) {
-            res.status(200).json({ message: 'Update successful', data: result });
-          } else {
-            res.status(404).json({ message: 'Agency not found' });
-          }
+    if (req.body.typeofuser == "agencies") {
+      const updateData = {
+        agencyName: req.body.agencyName,
+        agencyLogo: req.body.logo,
+        gstNumber: req.body.gstNumber,
+        legalName: req.body.legalName,
+        address: req.body.address,
+        gstCertificate: req.body.gstCertificate,
+        cinNumber: req.body.cinNumber,
+      };
 
-    }else{
-        const updateData = {
-            advertiserName: req.body.advertiserName,
-            gstNumber: req.body.gstNumber,
-            advertiserLogo :req.body.logo,
-            legalName: req.body.legalName,
-            address: req.body.address,
-            gstCertificate: req.body.gstCertificate,
-            cinNumber: req.body.cinNumber
-          };
-          const advertiserId = req.body.advertiserId;
-        
-        const result = await Advertisermodel.findOneAndUpdate({advertiserId: advertiserId},updateData,{new: true});
-        if (result) {
-            res.status(200).json({ message: 'Update successful', data: result });
-          } else {
-            res.status(404).json({ message: 'Advertiser not found' });
-          }
-      } 
-    }
-    catch (error) {
-        return res.status(401).json({ error: error });
+      const agencyId = req.body.agencyId;
+      const result = await Agencymodel.findOneAndUpdate(
+        { agencyId: agencyId },
+        updateData,
+        { new: true }
+      );
+      if (result) {
+        res.status(200).json({ message: "Update successful", data: result });
+      } else {
+        res.status(404).json({ message: "Agency not found" });
       }
-    
-   
+    } else {
+      const updateData = {
+        advertiserName: req.body.advertiserName,
+        gstNumber: req.body.gstNumber,
+        advertiserLogo: req.body.logo,
+        legalName: req.body.legalName,
+        address: req.body.address,
+        gstCertificate: req.body.gstCertificate,
+        cinNumber: req.body.cinNumber,
+      };
+      const advertiserId = req.body.advertiserId;
 
-   
-    
+      const result = await Advertisermodel.findOneAndUpdate(
+        { advertiserId: advertiserId },
+        updateData,
+        { new: true }
+      );
+      if (result) {
+        res.status(200).json({ message: "Update successful", data: result });
+      } else {
+        res.status(404).json({ message: "Advertiser not found" });
+      }
+    }
+  } catch (error) {
+    return res.status(401).json({ error: error });
+  }
 };
