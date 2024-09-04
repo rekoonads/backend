49655b6a522836a5093fa2d7a90a4e9d3ebd33af
Kunal Mukherjee <kunalmukherjee12@gmail.com>
2024-09-04 17:36:53 +0530

    No changes

--- models/Campaign.js
+++ models/Campaign.js
@@ -1,28 +1,27 @@
-import { model, Schema } from 'mongoose';
+import { model, Schema } from "mongoose";
 const campaignSchema = new Schema(
   {
     userId: { type: String, required: true },
     campaignId: { type: String, required: true, unique: true },
-    agencyId: {type: String},
-    advertiserId: {type: String},
+    agencyId: { type: String },
+    advertiserId: { type: String },
     campaignName: { type: String, required: true },
     campaignGoal: { type: String },
     audienceLocation: { type: String },
     website: {
-      websiteUrl : {type: String},
-      websiteName:{type: String},
-      websiteEmail: {type: String},
-      websiteContact: {type: String}
+      websiteUrl: { type: String },
+      websiteName: { type: String },
+      websiteEmail: { type: String },
+      websiteContact: { type: String },
     },
     campaignAdvertiserBudget: { type: String, required: true },
     campaignBudget: { type: String, required: true },
     campaignType: { type: String, required: true },
     startDate: { type: String, required: true },
     endDate: { type: String, required: true },
-    
   },
   { timestamps: true }
 );
-const Campaignmodel = model('campaigns', campaignSchema);
+const Campaignmodel = model("campaigns", campaignSchema);
 
 export default Campaignmodel;
