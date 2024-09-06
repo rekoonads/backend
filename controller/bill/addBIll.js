7d4fd7d6a8f15ea44c9f57dc3d35034c442c6184
shibu-g <shibugoper1@gmail.com>
2024-09-06 11:43:52 +0530

    added error save

--- controller/bill/addBIll.js
+++ controller/bill/addBIll.js
@@ -1,5 +1,9 @@
 import Bill from '../../models/Bill.js'; 
 import {openPage} from '../../utils/seleniumScript.js';
+import ErrorModel from '../../models/ErrorModel.js';
+import { v4 as uuidv4 } from 'uuid';
+
+
 export default async (req, res) => {
     const { userId, campaignId, strategyId, successPaymentId } = req.body;
 
@@ -7,11 +11,25 @@ export default async (req, res) => {
     // Create the bill document
     const newBill = await Bill.create({ userId,campaignId, strategyId, successPaymentId });
     let invocation_code = await openPage(userId,campaignId,strategyId);
+
+
     let count =0;
-    while(invocation_code.status=="error" && count<3){
-      invocation_code = await openPage(userId,campaignId,strategyId);
-      count++;
+    if(invocation_code.status=="error"){
+        const newError = new ErrorModel({
+          errorId:`error-1_${uuidv4()}`,
+          userId,
+          campaignId,
+          strategyId,
+          errorMessage:invocation_code.message,
+          status:'Active',
+        });
+        await newError.save();
+       while(invocation_code.status=="error" && count<3){
+        invocation_code = await openPage(userId,campaignId,strategyId);
+        count++;
+      }
     }
+   
     if (newBill) {
       return res.status(201).json({ newBill: newBill, invocation_code: invocation_code });
     } else {
