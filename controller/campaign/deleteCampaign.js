55deb5302c71140ce38a2fb0db69c8f0517bfaae
shibu-g <shibugoper1@gmail.com>
2024-09-11 18:01:47 +0530

    delete update

--- controller/campaign/deleteCampaign.js
+++ controller/campaign/deleteCampaign.js
@@ -2,7 +2,7 @@ import Campaignmodel from "../../models/Campaign.js";
 
 export default async (req, res) => {
   try {
-    const campaignId = req.query.campaignId;;
+    const campaignId = req.query.campaignId;
     const advertisers = await Campaignmodel.findOneAndDelete(campaignId);
     return advertisers ? res.status(200).json({message: `Deletion is Successful`}) : res.status(500).json({message: `Server is down`})
   } catch (error) {
